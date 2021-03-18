fd404b7b76b2b5511edc476d11234025776fa30e
Vasile Moraru <vasile.moraru@amdaris.com>
2021-03-18 11:53:26 +0200

    Initial commit

---
+++ Basics/Units/TodoItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basics.Units
+{
+    public partial class TodoList
+    {
+        public record TodoItem(string Content)
+        {
+            public int Id { get; init; }
+            public bool Complete { get; init; }
+        }
+    
+    }
+}
---
+++ Basics/Units/TodoList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Basics.Units
+{
+    public partial class TodoList
+    {
+
+        private readonly List<TodoItem> _todoItems = new();
+        private int idCounter = 1;
+
+        public void Add(TodoItem item)
+        {
+            _todoItems.Add(item with {Id = idCounter++});
+        }
+
+        public IEnumerable<TodoItem> All => _todoItems;
+
+        public void Complete(int id)
+        {
+            var item = _todoItems.First(x => x.Id == id);
+            _todoItems.Remove(item);
+
+            var completedItem = item with {Complete = true};
+            _todoItems.Add(completedItem);
+        }
+    }
+}
